d0dbd58ed28bd0b723d01b02cdc44a2a60027300
Abdel-Crime <Mohamed.arrahmani05@gmail.com>
2023-05-13 10:34:47 +0200

    V2.2

--- API/app.js
+++ API/app.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 4200;
+const cors = require('cors');
+//-------------------------------------------------------------------------------------------
+const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('mongodb');
+const bodyParser = require('body-parser');
+const signIn = require('./pages/login-get');
+const login = require('./pages/login-get');
+const logout = require('./pages/login-get');
+const isConnected = require('./pages/login-get');
+const getUser = require('./pages/login-get');
+const session = require('express-session');
+//-------------------------------------------------------------------------------------------
+const voitureGet = require('./pages/voiture-get');
+const voiturePost = require('./pages/voiture-get');
+const voitureDelete = require('./pages/voiture-get');
+const voiturePut = require('./pages/voiture-get');
+//-------------------------------------------------------------------------------------------
 const genererPageAccueil = require('./pages/index-get');
 const genererPageContact = require('./pages/contact-get');
 const genererPageAboutUS = require('./pages/aboutUs-get');
 const genererPageLogin = require('./pages/login-get');
+//-------------------------------------------------------------------------------------------
+app.use(cors({ credentials: true, origin: 'http://localhost:4200' }));
+
 
 app.get('/index.html', async(req, res) => {
     const indexHtml = await genererPageAccueil();
@@ -30,6 +50,43 @@ app.use('/CSS',express.static('../CSS'));
 app.use('/ADD',express.static('../ADD'));
 app.use('/IMG',express.static('../IMG'));
 
+//--------------------------------------------------------------------------
+
+app.use(session({
+    secret: "chut, c'est un secret",
+    name: "cookieTacheApplication"
+}));
+
+function checkSignIn(req, res, next) {
+    if (req.session.user) {
+        next(); //Si la session exist on passe au handler normal.
+    } else {
+        // res.status(401).send("Unauthorized");
+        next();
+    }
+}
+
+app.post('/login', login);
+app.post('/signIn',signIn)
+app.post('/logout', logout);
+//---------------------------------------------------------------
+app.get('/voiture.component.html',checkSignIn,async(req, res) => {
+    const voitureGet = await voitureGet.voitureGet();
+    res.send(voitureGet);
+});
+app.post('/voiture', checkSignIn, async(req, res) => {
+    const voiturePost = await voitureGet.voiturePost();
+    res.send(voiturePost);
+});
+app.delete('/voiture/:id', checkSignIn, async(req, res) => {
+    const voitureDelete = await voitureGet.voitureDelete();
+    res.send(voitureDelete);
+});
+app.put('/voiture/:id', checkSignIn, async(req, res) => {
+    const voiturePut = await voitureGet.voiturePut();
+    res.send(voiturePut);
+});
+//--------------------------------------------------------------
 app.listen(PORT,()=>{
     console.log(`L'application écoute le port ${PORT}`);
 });
--- API/pages/login-get.js
+++ API/pages/login-get.js
@@ -2,6 +2,10 @@ const { readFile } = require('fs');
 const {promisify} = require('util');
 const readFileAsync = promisify(readFile);
 
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const url = "mongodb://127.0.0.1:27017/";
+
 const READ_OPTION = { encoding: 'UTF-8'}
 
 module.exports = async() => {
@@ -9,4 +13,62 @@ module.exports = async() => {
 
 
     return contenu;
+}
+
+exports.signIn = async function (req, res) {
+    let utilisateur = req.body;
+    console.log(req.body+"C'est le user crée")
+    try {
+        db = await MongoClient.connect(url);
+        let dbo = db.db("taches");
+        let erreur=false
+        liste_user=await dbo.collection("utilisateur").find({}).toArray();
+        liste_user.forEach(element => {
+            if(element.login==utilisateur.login){
+                erreur=true
+            }
+            
+        });
+        if(erreur===false){
+            await dbo.collection("utilisateur").insertOne(utilisateur);
+            res.status(200).send();
+        }
+        else{
+            
+            res.status(401).json({ message: 'Unauthorized' });
+        }
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: err })
+    }
+}
+
+exports.login = async function (req, res) {
+    try {
+        console.log("zerty")
+        let utilisateur = req.body;
+        db = await MongoClient.connect(url);
+        let dbo = db.db("taches");
+        let utilisateurs = await dbo.collection("utilisateur").find({ login: utilisateur.login, password: utilisateur.password }).toArray();
+        if (utilisateurs.length > 0) {
+            req.session.user = utilisateurs[0].login;
+            res.status(200).send();
+        } else {
+            res.status(401).json({ message: 'Unauthorized' });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({ message: err })
+    }
+}
+
+exports.logout = async function (req, res) {
+    if (req.session)
+        await req.session.destroy();
+    res.status(200).end();
+}
+
+exports.isConnected = async function (req, res) {
+    res.status(200).end();
 }
